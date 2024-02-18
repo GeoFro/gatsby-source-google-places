32cc0fb9eba489138b0d79a3ee483d2add1e74ee
GeoFro <georgeharoldfrost@gmail.com>
2024-02-18 00:44:05 +0000

    handle images for GatsbyImage

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,52 +1,82 @@
-const getPlaceDetails = require('./getPlaceDetails');
-const nodeFactory = require('./nodeFactory');
+const getPlaceDetails = require("./getPlaceDetails");
+const nodeFactory = require("./nodeFactory");
 
-const resolvePlace = async ({ apiKey, placeId, language, createNode }) => {
-  const response = (await getPlaceDetails(apiKey, placeId, language));
+const resolvePlace = async ({
+  apiKey,
+  placeId,
+  language,
+  actions,
+  createNodeId,
+  store,
+  cache,
+  reporter,
+}) => {
+  const response = await getPlaceDetails(apiKey, placeId, language);
 
   if (response.data.status !== "OK") {
-    throw Error("Request to Google API failed. " + response.data.error_message)
+    throw new Error(
+      "Request to Google API failed. " + response.data.error_message
+    );
   }
 
-  const place = response.data.result
-
+  const place = response.data.result;
   place.id = placeId;
 
   const placeNode = nodeFactory.placeNode(place);
-  delete placeNode.reviews
-  createNode(placeNode);
-  if(place.reviews){
-  place.reviews.forEach(review => {
-    review.id = review.time;
-    const placeReviewNode = nodeFactory.reviewNode(review, {
-      parent: placeNode.id
-    });
-    createNode(placeReviewNode);
-  })}
+  delete placeNode.reviews;
+  actions.createNode(placeNode);
+
+  if (place.reviews) {
+    for (const review of place.reviews) {
+      review.id = review.time.toString();
+      // Now passing additional parameters to `reviewNode`
+      const reviewNode = await nodeFactory.reviewNode(review, {
+        createNode: actions.createNode,
+        createNodeId,
+        store,
+        cache,
+        reporter,
+      });
+      actions.createNode(reviewNode);
+    }
+  }
 
   return;
-}
+};
 
-exports.sourceNodes = async ({ actions }, { apiKey, placeIds, placeId, language = 'en-US' }) => {
+exports.sourceNodes = async (
+  { actions, createNodeId, store, cache, reporter },
+  { apiKey, placeIds, placeId, language = "en-US" }
+) => {
   try {
-
     // Backwards compatibility
     if (!placeIds) {
-      console.warn('gatsby-source-google-places has deprecated placeId. Use placeIds instead.')
-      placeIds = placeId
+      console.warn(
+        "gatsby-source-google-places has deprecated placeId. Use placeIds instead."
+      );
+      placeIds = placeId;
     }
 
-    const { createNode } = actions;
-
-    // Single Place Id
     if (typeof placeIds === "string") {
-      placeIds = [placeIds]
+      placeIds = [placeIds];
     }
 
-    await Promise.all(placeIds.map((pid) => resolvePlace({ apiKey, placeId: pid, language, createNode })))
-    
+    await Promise.all(
+      placeIds.map((pid) =>
+        resolvePlace({
+          apiKey,
+          placeId: pid,
+          language,
+          actions,
+          createNodeId,
+          store,
+          cache,
+          reporter,
+        })
+      )
+    );
   } catch (error) {
-    console.error(error)
-    process.exit(1)
+    console.error(error);
+    process.exit(1);
   }
-}
+};
--- src/nodeFactory.js
+++ src/nodeFactory.js
@@ -1,25 +1,62 @@
-const createNodeHelpers = require('gatsby-node-helpers').default;
+const { createNodeFactory } = require("gatsby-node-helpers").default;
+const { createRemoteFileNode } = require(`gatsby-source-filesystem`);
 
-const {
-  createNodeFactory,
-  generateNodeId,
-} = createNodeHelpers({
+const PLACE_TYPE = `Place`;
+const REVIEW_TYPE = `Review`;
+
+const { createNodeHelpers } = require("gatsby-node-helpers")({
   typePrefix: `GooglePlaces`,
-})
+});
 
-const PLACE_TYPE = `Place`
-const REVIEW_TYPE = `Review`
+const { generateNodeId } = createNodeHelpers;
 
-module.exports.placeNode = createNodeFactory(PLACE_TYPE, node => {
+const placeNode = createNodeFactory(PLACE_TYPE, (node) => {
+  // Process place node (e.g., link children reviews)
   if (node.reviews) {
-    const reviews = node.reviews.map(review => review)
-    node.children = reviews.map(review =>
-      generateNodeId(REVIEW_TYPE, review.time),
-    )
+    node.children = node.reviews.map((review) =>
+      generateNodeId(REVIEW_TYPE, review.time.toString())
+    );
   }
-  return node 
-})
+  return node;
+});
 
-module.exports.reviewNode = createNodeFactory(REVIEW_TYPE, node => {
-  return node
-})
+const reviewNode = (
+  node,
+  { createNode, createNodeId, store, cache, reporter }
+) => {
+  // This is a custom function, not directly using `createNodeFactory` because we need to perform async operations
+  return (async () => {
+    if (node.profile_photo_url) {
+      try {
+        // Download the profile photo and create a File node
+        const fileNode = await createRemoteFileNode({
+          url: node.profile_photo_url,
+          parentNodeId: node.id,
+          createNode, // passed from gatsby's actions
+          createNodeId,
+          cache,
+          store,
+          reporter,
+        });
+
+        // If the file was created successfully, link it to the review node
+        if (fileNode) {
+          node.localProfilePhoto___NODE = fileNode.id; // This links the File node to the Review node
+        }
+      } catch (error) {
+        reporter.warn(
+          `Error downloading image: ${node.profile_photo_url}`,
+          error
+        );
+      }
+    }
+
+    // Finally, return the modified node
+    return node;
+  })();
+};
+
+module.exports = {
+  placeNode,
+  reviewNode: createNodeFactory(REVIEW_TYPE, reviewNode), // Wrap `reviewNode` with `createNodeFactory` to ensure it's processed correctly
+};
